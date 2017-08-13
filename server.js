3d7115e29f76cb8207ea593fd4b4b1d1283af8fd
Kartheek Rao <30475093+kartikrao65@users.noreply.github.com>
2017-08-13 09:30:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req,res) {
-    res.send('article one will be requested and will be served sir');
-});
+res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));});
 
  app.get('/article_two',  function (req,res) {
     res.send('article two will be requested and will be served sir');
